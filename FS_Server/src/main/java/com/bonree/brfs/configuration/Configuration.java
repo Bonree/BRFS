12bdca37040ddbba38162b197c680244d4ced5ae
zhwei4150@126.com <zhwei4150@126.com>
2018-03-13 11:33:39 +0800

    update
    update

---
+++ FS_Server/src/main/java/com/bonree/brfs/configuration/Configuration.java
@@ -0,0 +1,17 @@
+package com.bonree.brfs.configuration;
+
+public class Configuration {
+
+    public static String CLUSTER_NAME = "cluster.name";
+
+    public static String CLUSTER_NAME_VALUE = "default_brfs";
+
+    public static String DEFAULT_STORAGE_COMBINER_FILE_MAX_SIZE = "default.storage.combiner.file.max.size";
+
+    public static String DEFAULT_STORAGE_COMBINER_FILE_MAX_SIZE_VALUE = "64";
+
+    public static String DEFAULT_STORAGE_DATA_TTL = "default.storage.data.ttl";
+    
+    public static String DEFAULT_STORAGE_DATA_TTL
+
+}
--- FS_Server/src/test/java/com/bonree/brfs/zookeeper/curator/LockTest.java
+++ FS_Server/src/test/java/com/bonree/brfs/zookeeper/curator/LockTest.java
@@ -2,8 +2,5 @@ package com.bonree.brfs.zookeeper.curator;
 
 public class LockTest {
 
-    public LockTest() {
-        // TODO Auto-generated constructor stub
-    }
 
 }
