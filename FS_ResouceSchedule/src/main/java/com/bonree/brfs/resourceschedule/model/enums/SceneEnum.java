af056aaa6d55a6317f92f99dde7ea8bdc73f0110
wellicoming <wellicoming@126.com>
2018-03-16 15:34:25 +0800

    增加场景枚举

---
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/model/enums/SceneEnum.java
@@ -0,0 +1,9 @@
+package com.bonree.brfs.resourceschedule.model.enums;
+
+public enum SceneEnum {
+	CLIENT_WRITE_REQUEST,
+	CLIENT_READ_REQUEST,
+	CLIENT_META_REQUEST,
+	TASK_RUNNABLE_REQUEST,
+	TASK_RUNPATTERN_REQUEST
+}
