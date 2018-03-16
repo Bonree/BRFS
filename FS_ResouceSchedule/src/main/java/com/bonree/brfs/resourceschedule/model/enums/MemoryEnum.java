495986740b82c2eb55491743691ec42cbed84b2a
wellicoming <wellicoming@126.com>
2018-03-16 11:50:13 +0800

    拆分枚举，每个枚举为一个文件

---
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/model/enums/MemoryEnum.java
@@ -0,0 +1,7 @@
+package com.bonree.brfs.resourceschedule.model.enums;
+
+public enum MemoryEnum {
+	MEMORY_SIZE,
+	MEMORY_RATE,
+	MEMORY_REMAIN_RATE
+}
---
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/model/enums/PatitionEnum.java
@@ -0,0 +1,14 @@
+package com.bonree.brfs.resourceschedule.model.enums;
+
+public enum PatitionEnum {
+	MOUNT_POINT,
+	PATITION_FORMAT,
+	DISK_TYPE,
+	PATITION_SIZE,
+	MAX_WRITE_SPEED,
+	MAX_READ_SPEED,
+	USED_SIZE,
+	REMAIN_SIZE,
+	WIRTE_DATA_SIZE,
+	READ_DATA_SIZE
+}
