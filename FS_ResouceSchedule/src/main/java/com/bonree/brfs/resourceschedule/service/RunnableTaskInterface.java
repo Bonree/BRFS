d3da6e4d571c44c58950e426104d78d1c42fb4e4
wellicoming <wellicoming@126.com>
2018-03-21 15:44:15 +0800

    添加任务可用接口

---
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/service/RunnableTaskInterface.java
@@ -0,0 +1,13 @@
+package com.bonree.brfs.resourceschedule.service;
+/*****************************************************************************
+ * 版权信息：北京博睿宏远数据科技股份有限公司
+ * Copyright: Copyright (c) 2007北京博睿宏远数据科技股份有限公司,Inc.All Rights Reserved.
+ * 
+ * @date 2018年3月16日 上午10:51:07
+ * @Author: <a href=mailto:zhucg@bonree.com>朱成岗</a>
+ * @Description: 任务资源管理接口
+ *****************************************************************************
+ */
+public interface RunnableTaskInterface {
+
+}
