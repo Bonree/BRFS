7f7cfee00f030df0c746d78591859df05ea847c5
wellicoming <wellicoming@126.com>
2018-03-23 15:30:41 +0800

    修改缓存成员变量

--- FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/commons/Cache.java
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/commons/Cache.java
@@ -2,9 +2,14 @@ package com.bonree.brfs.resourceschedule.commons;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.jboss.netty.util.internal.ConcurrentHashMap;
 
 import com.bonree.brfs.resourceschedule.model.BaseServerModel;
-import com.bonree.brfs.resourceschedule.model.ServerModel;
+import com.bonree.brfs.resourceschedule.model.ServerStatModel;
 
 /*****************************************************************************
  * 版权信息：北京博睿宏远数据科技股份有限公司
@@ -31,17 +36,26 @@ public class Cache {
 	 * 磁盘最大读取速度 单位byte（按500MB/s的ssd硬盘计算）
 	 */
 	public static long DISK_MAX_READ_SPEED = 616448000L;
-	/**
-	 * 数据缓存
-	 */
-	/**
-	 * 服务器状态
-	 */
-	public ServerModel SERVER_INFO;
+    /**
+     * 服务基本信息
+     */
+    public BaseServerModel baseInfo = new BaseServerModel();
+    /**
+     * 服务状态信息
+     */
+    public Queue<ServerStatModel> statInfoQueue = new ConcurrentLinkedQueue<ServerStatModel>();
 	/**
 	 * 服务器基础信息
 	 */
-	public List<BaseServerModel> BASE_CLUSTER_INFO = new ArrayList<BaseServerModel>(); 
+	public List<BaseServerModel> BASE_CLUSTER_INFO = new ArrayList<BaseServerModel>();
+	/**
+	 * sn与磁盘的对应关系 key：sn根目录，value：分区挂载点
+	 */
+	public Map<String,String> snWithDisk = new ConcurrentHashMap<String,String>();
+	/**
+	 * sn剩余空间大小，Key：sn根目录，value：剩余大小
+	 */
+	public Map<String,Long> snRemainSizeMap = new ConcurrentHashMap<String,Long>();
 	
 	/**
 	 * 服务数据目录
