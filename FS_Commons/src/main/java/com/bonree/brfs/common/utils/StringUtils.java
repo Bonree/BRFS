dc26fda30d71e2038855e7a003c4ab9ba3f9218e
wellicoming <wellicoming@126.com>
2018-03-28 16:05:20 +0800

    增加方法
    1.增加字符串切分数组方法
    2.增加判断为空方法

--- FS_Commons/src/main/java/com/bonree/brfs/common/utils/StringUtils.java
+++ FS_Commons/src/main/java/com/bonree/brfs/common/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package com.bonree.brfs.common.utils;
 
 import java.io.UnsupportedEncodingException;
+import java.util.StringTokenizer;
 
 public class StringUtils {
 
@@ -57,4 +58,60 @@ public class StringUtils {
 		
 		return new byte[0];
 	}
+    /**
+     * 判断字符串是否为空
+     *
+     * @param args 字符串
+     * @return 空 true,不为空 false
+     * @author 张念礼
+     * @date 2012-4-13 上午10:05:55
+     * @version V1.0.0
+     * @ModifyRecord 修改记录
+     * <br>1、张念礼 ; 2012-4-13 上午10:05:55; 初始化
+     */
+    public static final Boolean isEmpty(String args) {
+        Boolean bool = false;
+        //空对象
+        if (null == args) {
+            bool = true;
+        }
+        //空字符串
+        else if ("".equals(args.trim())) {
+            bool = true;
+        }
+        //全角
+        else if ("　".equals(args)) {
+            bool = true;
+        }
+        //null字符串
+        else if ("null".equals(args)) {
+            bool = true;
+        } else if ("[]".equals(args)) {
+            bool = true;
+        }
+        return bool;
+    }
+    
+    /**
+     * 概述：分割字符串
+     *
+     * @param str   字段串
+     * @param delim 分割符号
+     * @return String[]
+     * @Title: getSplit
+     * @user <a href=mailto:zhangnl@bonree.com>张念礼</a>
+     */
+    public static final String[] getSplit(String str, String delim) {
+        if (str == null || delim == null) {
+            return null;
+        }
+        StringTokenizer token = new StringTokenizer(str, delim);
+        int num = token.countTokens();
+        String[] result = new String[num];
+        int i = 0;
+        while (token.hasMoreTokens()) {
+            result[i++] = token.nextToken();
+        }
+        return result;
+    }
 }
