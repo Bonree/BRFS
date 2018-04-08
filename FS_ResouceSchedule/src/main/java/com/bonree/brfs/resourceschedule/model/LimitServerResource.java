ecc4abc37e2198a960dfdbe01235fec3b5244fbd
wellicoming <wellicoming@126.com>
2018-04-08 13:54:04 +0800

    重构资源调度
    1.修改采集资源类
    2增加策略类

---
+++ FS_ResouceSchedule/src/main/java/com/bonree/brfs/resourceschedule/model/LimitServerResource.java
@@ -0,0 +1,15 @@
+package com.bonree.brfs.resourceschedule.model;
+
+public class LimitServerResource {
+	
+	/**
+	 * 本机最大硬盘剩余率
+	 */
+	private double remainValue;
+	public double getRemainValue() {
+		return remainValue;
+	}
+	public void setRemainValue(double remainValue) {
+		this.remainValue = remainValue;
+	}
+}
