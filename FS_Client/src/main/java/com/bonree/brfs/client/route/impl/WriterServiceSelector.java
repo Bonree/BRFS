9861ce50e0ba706a845de8cc114cbf430bb1be5d
zhwei4150@126.com <zhwei4150@126.com>
2018-05-08 15:37:16 +0800

    update
    update

--- FS_Client/src/main/java/com/bonree/brfs/client/route/impl/WriterServiceSelector.java
+++ FS_Client/src/main/java/com/bonree/brfs/client/route/impl/WriterServiceSelector.java
@@ -1,5 +1,9 @@
 package com.bonree.brfs.client.route.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.bonree.brfs.client.meta.ServiceMetaCache;
 import com.bonree.brfs.client.route.ServiceSelector;
 import com.bonree.brfs.common.service.Service;
@@ -8,8 +12,13 @@ public class WriterServiceSelector implements ServiceSelector {
 
     @Override
     public Service selectService(ServiceMetaCache serviceCache) {
-        String perfectFirstID = null;
-        return serviceCache.getFirstServerCache(perfectFirstID);
+//        String perfectFirstID = null;
+//        return serviceCache.getFirstServerCache(perfectFirstID);
+        
+        List<String> firstIDs = new ArrayList<String>(serviceCache.getFirstServerCache().keySet());
+        Random random = new Random();
+        String randomFirstID = firstIDs.get(random.nextInt(firstIDs.size()));
+        return serviceCache.getFirstServerCache().get(randomFirstID);
     }
 
 }
