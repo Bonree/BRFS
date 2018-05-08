2dd13fee0c09b0ef6ff91bf8cb73230654893cda
zhwei4150@126.com <zhwei4150@126.com>
2018-05-08 15:32:16 +0800

    update
    update

--- FS_Client/src/main/java/com/bonree/brfs/client/route/ServiceSelector_1.java
+++ FS_Client/src/main/java/com/bonree/brfs/client/route/ServiceSelector_1.java
@@ -5,8 +5,10 @@ import com.bonree.brfs.common.service.Service;
 
 public interface ServiceSelector_1 {
 
-    /** 概述：选择一个Service
-     * @param params
+    /** 概述：
+     * @param serviceCache
+     * @param routeParser
+     * @param partFid UUID_s1_s2
      * @return
      * @user <a href=mailto:weizheng@bonree.com>魏征</a>
      */
