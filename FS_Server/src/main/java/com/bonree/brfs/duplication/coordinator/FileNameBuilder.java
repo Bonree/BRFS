64333ffe89571d5d856981684ff3e2ef815f880d
ChenYupeng <chenyp@bonree.com>
2018-05-09 14:23:23 +0800

    use uuid as a file name

--- FS_Server/src/main/java/com/bonree/brfs/duplication/coordinator/FileNameBuilder.java
+++ FS_Server/src/main/java/com/bonree/brfs/duplication/coordinator/FileNameBuilder.java
@@ -1,13 +1,10 @@
 package com.bonree.brfs.duplication.coordinator;
 
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FileNameBuilder {
-	private static AtomicInteger id = new AtomicInteger(0);
 	
 	public static String createFile() {
-//		return UUID.randomUUID().toString();
-		return "file_" + id.getAndIncrement();
+		return UUID.randomUUID().toString();
 	}
 }
