de26fb53c4d32563e6167d4f4b5cc2ab8535accd
wellicoming <wellicoming@126.com>
2018-05-13 16:06:47 +0800

    去掉配置项
    去掉资源采集依赖配置项，改为启动脚本参数传入

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -11,12 +11,14 @@ LOG_DIR=$basedir'logs/fs_server'
 HEAPSIZE='-Xms512m -Xmx1024m'
 
 GC_PATH='-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:'$basedir'logs/gc_server.log'
+#资源管理lib路径
+RESOURCE_LIB_PATH=$basedir'lib'
 
 if [ `jps | grep FS_Server.jar | wc -l` -gt 0 ]; then
    echo "warn: FS_Server is already running, please stop it first"
    exit -1
 fi
 
-nohup java -Dlog_dir=$LOG_DIR -jar $HEAPSIZE $GC_PATH $SERVER >/dev/null 2>&1 &
+nohup java -Dlog_dir=$LOG_DIR -Dresource_lib_path=$RESOURCE_LIB_PATH -jar $HEAPSIZE $GC_PATH $SERVER >/dev/null 2>&1 &
 
 echo -e "FS_Server has started.\nThe number of services running on this machine is `jps | grep FS_Server.jar | wc -l`."
