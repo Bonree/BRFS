ac6a2f5e06feefe113612f4136a25bb100ff2b1a
ChenYupeng <chenyp@bonree.com>
2018-06-14 22:39:11 +0800

    修复CRC计算的非线程安全导致的问题

--- FS_Commons/src/main/java/com/bonree/brfs/common/utils/ByteUtils.java
+++ FS_Commons/src/main/java/com/bonree/brfs/common/utils/ByteUtils.java
@@ -2,15 +2,14 @@ package com.bonree.brfs.common.utils;
 
 import java.util.zip.CRC32;
 
-public final class ByteUtils {
-	private static CRC32 crc32 = new CRC32();
+public final class ByteUtils {;
 	
 	public static long crc(byte[] bytes) {
 		return crc(bytes, 0, bytes.length);
 	}
 	
 	public static long crc(byte[] bytes, int offset, int length) {
-		crc32.reset();
+		CRC32 crc32 = new CRC32();
 		crc32.update(bytes, offset, length);
 		
 		return crc32.getValue();
