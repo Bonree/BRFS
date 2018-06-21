45f27d14a5319e4680513374dd3e1d1bede7f79b
wellicoming <wellicoming@126.com>
2018-06-21 22:03:05 +0800

    do

--- FS_Server/src/main/java/com/bonree/brfs/schedulers/MetaTaskLeaderManager.java
+++ FS_Server/src/main/java/com/bonree/brfs/schedulers/MetaTaskLeaderManager.java
@@ -179,8 +179,7 @@ public class MetaTaskLeaderManager implements LeaderLatchListener {
 		}
 
 		Map content = JobDataMapConstract.createCylcCheckDataMap(this.config.getCheckTimeRange());
-		SumbitTaskInterface sumbit = QuartzCronInfo.getInstance("CYCLE_CHECK_JOB", "CYCLE_CHECK_JOB",
-			this.config.getCheckCronStr(), content, CheckCycleJob.class);
+		SumbitTaskInterface sumbit = QuartzCronInfo.getInstance("CYCLE_CHECK_JOB", "CYCLE_CHECK_JOB", this.config.getCheckCronStr(), content, CheckCycleJob.class);
 		cFlag = this.manager.addTask("COPY_CYCLE_POOL", sumbit);
 		LOG.info("sumbit Cycle task :{}", Boolean.valueOf(cFlag));
 	}
