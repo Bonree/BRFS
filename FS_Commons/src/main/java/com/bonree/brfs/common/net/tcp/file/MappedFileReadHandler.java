ac2f9287d98a1316cdedf6836967bf8256125643
wellgeek <wellicoming@126.com>
2018-12-17 19:58:07 +0800

    修复读取乱码bug

--- FS_Commons/src/main/java/com/bonree/brfs/common/net/tcp/file/MappedFileReadHandler.java
+++ FS_Commons/src/main/java/com/bonree/brfs/common/net/tcp/file/MappedFileReadHandler.java
@@ -1,6 +1,7 @@
 package com.bonree.brfs.common.net.tcp.file;
 
 import com.bonree.brfs.common.utils.ByteUtils;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
@@ -35,216 +36,201 @@ import com.google.common.io.Files;
 import com.google.common.primitives.Ints;
 
 @Sharable
-public class MappedFileReadHandler extends SimpleChannelInboundHandler<ReadObject> {
-	private static final Logger LOG = LoggerFactory.getLogger(MappedFileReadHandler.class);
-	
-	private ReadObjectTranslator translator;
-	private ExecutorService releaseRunner = Executors.newSingleThreadExecutor(new ThreadFactory() {
-		
-		@Override
-		public Thread newThread(Runnable r) {
-			Thread thread = new Thread(r, "map_release_t");
-			thread.setDaemon(true);
-			
-			return thread;
-		}
-	});
-	
-	private LinkedList<BufferRef> releaseList = new LinkedList<>();
-	private LoadingCache<String, BufferRef> bufferCache = CacheBuilder.newBuilder()
-			.concurrencyLevel(Runtime.getRuntime().availableProcessors())
-			.maximumSize(64)
-			.initialCapacity(32)
-			.expireAfterAccess(30, TimeUnit.SECONDS)
-			.removalListener(new RemovalListener<String, BufferRef>() {
+public class MappedFileReadHandler extends SimpleChannelInboundHandler<ReadObject>{
+    private static final Logger LOG = LoggerFactory.getLogger(MappedFileReadHandler.class);
 
-				@Override
-				public void onRemoval(RemovalNotification<String, BufferRef> notification) {
-					LOG.info("remove file mapping of [{}] from cache", notification.getKey());
-					synchronized (releaseList) {
-						releaseList.addLast(notification.getValue());
-					}
-				}
-			})
-			.build(new CacheLoader<String, BufferRef>() {
+    private ReadObjectTranslator translator;
+    private ExecutorService releaseRunner = Executors.newSingleThreadExecutor(new ThreadFactory(){
 
-				@Override
-				public BufferRef load(String filePath) throws Exception {
-					LOG.info("loading file[{}] to memory...", filePath);
-					return new BufferRef(Files.map(new File(filePath), MapMode.READ_ONLY));
-				}
-				
-			});
-    
-    private LoadingCache<TimePair, String> timeCache = CacheBuilder.newBuilder()
-    		.maximumSize(1024)
-    		.build(new CacheLoader<TimePair, String>() {
+        @Override
+        public Thread newThread(Runnable r){
+            Thread thread = new Thread(r, "map_release_t");
+            thread.setDaemon(true);
 
-				@Override
-				public String load(TimePair pair) throws Exception {
-					return TimeUtils.timeInterval(pair.time, pair.duration);
-				}
-			});
-	
-	public MappedFileReadHandler(ReadObjectTranslator translator) {
-		this.translator = translator;
-		this.releaseRunner.execute(() -> {
-			while(true) {
-				Iterator<BufferRef> iter = releaseList.iterator();
-				while(iter.hasNext()) {
-					BufferRef bufferRef = iter.next();
-					if(bufferRef.refCount() == 0) {
-						BufferUtils.release(bufferRef.buffer());
-						iter.remove();
-					}
-				}
-				
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	
-	private String buildPath(ReadObject readObject) throws ExecutionException {
-		StringBuilder pathBuilder = new StringBuilder();
-		pathBuilder.append(File.separatorChar)
-		.append(readObject.getSn())
-		.append(File.separatorChar)
-		.append(readObject.getIndex())
-		.append(File.separatorChar)
-		.append(timeCache.get(new TimePair(TimeUtils.prevTimeStamp(readObject.getTime(), readObject.getDuration()), readObject.getDuration())))
-		.append(File.separatorChar)
-		.append(readObject.getFileName());
-		
-		return pathBuilder.toString();
-	}
+            return thread;
+        }
+    });
 
-	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, ReadObject readObject)throws Exception {
-		if(readObject.getFilePath().equals("-")) {
-			readObject.setFilePath(buildPath(readObject));
-		}
-		
-		String filePath = (readObject.getRaw() & ReadObject.RAW_PATH) == 0 ?
-				translator.filePath(readObject.getFilePath()) : readObject.getFilePath();
-		
-		MappedByteBuffer fileBuffer = null;
-		try {
-			BufferRef ref = bufferCache.get(filePath).retain();
-			fileBuffer = ref.buffer();
-			
-			long readOffset = (readObject.getRaw() & ReadObject.RAW_OFFSET) == 0 ? translator.offset(readObject.getOffset()) : readObject.getOffset();
-			int readLength = (readObject.getRaw() & ReadObject.RAW_LENGTH) == 0 ? translator.length(readObject.getLength()) : readObject.getLength();
-			long fileLength = fileBuffer.capacity();
-			if(readOffset < 0 || readOffset > fileLength) {
-				LOG.error("unexcepted file[{}] offset : {}, file length : {}", filePath, readOffset, fileLength);
-				ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1)))
-				.addListener(ChannelFutureListener.CLOSE);
-				return;
-			}
-			
-			int readableLength = (int) Math.min(readLength, fileLength - readOffset);
-			
-			ByteBuffer contentBuffer = fileBuffer.slice();
-			contentBuffer.position((int) readOffset);
-			contentBuffer.limit((int) (readOffset + readableLength));
+    private LinkedList<BufferRef> releaseList = new LinkedList<>();
+    private LoadingCache<String, BufferRef> bufferCache = CacheBuilder.newBuilder().concurrencyLevel(Runtime.getRuntime().availableProcessors()).maximumSize(64).initialCapacity(32).expireAfterAccess(30, TimeUnit.SECONDS).removalListener(new RemovalListener<String, BufferRef>(){
 
-			System.out.println("READ CRC == " + ByteUtils.cyc(contentBuffer.slice()));
-			
-			ctx.write(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(readableLength)));
-			ctx.writeAndFlush(Unpooled.wrappedBuffer(contentBuffer.slice())).addListener(new ChannelFutureListener() {
-				
-				@Override
-				public void operationComplete(ChannelFuture future) throws Exception {
-					ref.release();
-				}
-			}).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
-		} catch (ExecutionException e) {
-			LOG.error("can not open file channel for {}", filePath, e);
-			ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1)))
-			.addListener(ChannelFutureListener.CLOSE);
-			return;
-		} catch (Exception e) {
-			LOG.error("read file error", e);
-			ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1)))
-			.addListener(ChannelFutureListener.CLOSE);
-			return;
-		} finally {
-			bufferCache.cleanUp();
-		}
-	}
+        @Override
+        public void onRemoval(RemovalNotification<String, BufferRef> notification){
+            LOG.info("remove file mapping of [{}] from cache", notification.getKey());
+            synchronized(releaseList) {
+                releaseList.addLast(notification.getValue());
+            }
+        }
+    }).build(new CacheLoader<String, BufferRef>(){
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		LOG.error("file read error", cause);
-		ctx.close();
-	}
-	
-	private class BufferRef {
-		private AtomicInteger refCount;
-		private final MappedByteBuffer buffer;
-		
-		public BufferRef(MappedByteBuffer buffer) {
-			this.buffer = buffer;
-			this.refCount = new AtomicInteger(0);
-		}
-		
-		public MappedByteBuffer buffer() {
-			return this.buffer;
-		}
-		
-		public int refCount() {
-			return this.refCount.get();
-		}
-		
-		public BufferRef retain() {
-			refCount.incrementAndGet();
-			return this;
-		}
-		
-		public boolean release() {
-			return refCount.decrementAndGet() == 0;
-		}
-	}
-	
-	private static class TimePair {
-    	private final long time;
-    	private final long duration;
-    	
-    	public TimePair(long time, long duration) {
-    		this.time = time;
-    		this.duration = duration;
-    	}
-    	
-    	public long time() {
-    		return this.time;
-    	}
-    	
-    	public long duration() {
-    		return this.duration;
-    	}
-    	
-    	@Override
-		public int hashCode() {
-			return (int) (this.time * 37 + this.duration);
-		}
+        @Override
+        public BufferRef load(String filePath) throws Exception{
+            LOG.info("loading file[{}] to memory...", filePath);
+            return new BufferRef(Files.map(new File(filePath), MapMode.READ_ONLY));
+        }
 
-		@Override
-		public boolean equals(Object obj) {
-			if(obj == null) {
-				return false;
-			}
-			
-			if(!(obj instanceof TimePair)) {
-				return false;
-			}
-			
-			TimePair oth = (TimePair) obj; 
-			
-			return this.time == oth.time && this.duration == oth.duration;
-		}
+    });
+
+    private LoadingCache<TimePair, String> timeCache = CacheBuilder.newBuilder().maximumSize(1024).build(new CacheLoader<TimePair, String>(){
+
+        @Override
+        public String load(TimePair pair) throws Exception{
+            return TimeUtils.timeInterval(pair.time, pair.duration);
+        }
+    });
+
+    public MappedFileReadHandler(ReadObjectTranslator translator){
+        this.translator = translator;
+        this.releaseRunner.execute(() -> {
+            while(true) {
+                Iterator<BufferRef> iter = releaseList.iterator();
+                while(iter.hasNext()) {
+                    BufferRef bufferRef = iter.next();
+                    if(bufferRef.refCount() == 0) {
+                        BufferUtils.release(bufferRef.buffer());
+                        iter.remove();
+                    }
+                }
+
+                try{
+                    Thread.sleep(1000);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private String buildPath(ReadObject readObject) throws ExecutionException{
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.append(File.separatorChar).append(readObject.getSn()).append(File.separatorChar).append(readObject.getIndex()).append(File.separatorChar).append(timeCache.get(new TimePair(TimeUtils.prevTimeStamp(readObject.getTime(), readObject.getDuration()), readObject.getDuration()))).append(File.separatorChar).append(readObject.getFileName());
+
+        return pathBuilder.toString();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ReadObject readObject) throws Exception{
+        if(readObject.getFilePath().equals("-")) {
+            readObject.setFilePath(buildPath(readObject));
+        }
+
+        String filePath = (readObject.getRaw() & ReadObject.RAW_PATH) == 0 ? translator.filePath(readObject.getFilePath()) : readObject.getFilePath();
+
+        MappedByteBuffer fileBuffer = null;
+        try{
+            BufferRef ref = bufferCache.get(filePath).retain();
+            fileBuffer = ref.buffer();
+
+            long readOffset = (readObject.getRaw() & ReadObject.RAW_OFFSET) == 0 ? translator.offset(readObject.getOffset()) : readObject.getOffset();
+            int readLength = (readObject.getRaw() & ReadObject.RAW_LENGTH) == 0 ? translator.length(readObject.getLength()) : readObject.getLength();
+            long fileLength = fileBuffer.capacity();
+            if(readOffset < 0 || readOffset > fileLength) {
+                LOG.error("unexcepted file[{}] offset : {}, file length : {}", filePath, readOffset, fileLength);
+                ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1))).addListener(ChannelFutureListener.CLOSE);
+                return;
+            }
+
+            int readableLength = (int) Math.min(readLength, fileLength - readOffset);
+
+            ByteBuffer contentBuffer = fileBuffer.slice();
+            contentBuffer.position((int) readOffset);
+            contentBuffer.limit((int) (readOffset + readableLength));
+
+            System.out.println("READ CRC == " + ByteUtils.cyc(contentBuffer.slice()));
+
+            ctx.write(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(readableLength)));
+            CompositeByteBuf compositeByteBuf = Unpooled.compositeBuffer();
+            compositeByteBuf.addComponent(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(readableLength)));
+            compositeByteBuf.addComponent(Unpooled.wrappedBuffer(contentBuffer.slice()));
+
+            ctx.writeAndFlush(compositeByteBuf).addListener(new ChannelFutureListener(){
+
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception{
+                    ref.release();
+                }
+            }).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+        } catch(ExecutionException e){
+            LOG.error("can not open file channel for {}", filePath, e);
+            ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1))).addListener(ChannelFutureListener.CLOSE);
+            return;
+        } catch(Exception e){
+            LOG.error("read file error", e);
+            ctx.writeAndFlush(Unpooled.wrappedBuffer(Ints.toByteArray(readObject.getToken()), Ints.toByteArray(-1))).addListener(ChannelFutureListener.CLOSE);
+            return;
+        } finally{
+            bufferCache.cleanUp();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception{
+        LOG.error("file read error", cause);
+        ctx.close();
+    }
+
+    private class BufferRef{
+        private AtomicInteger refCount;
+        private final MappedByteBuffer buffer;
+
+        public BufferRef(MappedByteBuffer buffer){
+            this.buffer = buffer;
+            this.refCount = new AtomicInteger(0);
+        }
+
+        public MappedByteBuffer buffer(){
+            return this.buffer;
+        }
+
+        public int refCount(){
+            return this.refCount.get();
+        }
+
+        public BufferRef retain(){
+            refCount.incrementAndGet();
+            return this;
+        }
+
+        public boolean release(){
+            return refCount.decrementAndGet() == 0;
+        }
+    }
+
+    private static class TimePair{
+        private final long time;
+        private final long duration;
+
+        public TimePair(long time, long duration){
+            this.time = time;
+            this.duration = duration;
+        }
+
+        public long time(){
+            return this.time;
+        }
+
+        public long duration(){
+            return this.duration;
+        }
+
+        @Override
+        public int hashCode(){
+            return (int) (this.time * 37 + this.duration);
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            if(obj == null) {
+                return false;
+            }
+
+            if(!(obj instanceof TimePair)) {
+                return false;
+            }
+
+            TimePair oth = (TimePair) obj;
+
+            return this.time == oth.time && this.duration == oth.duration;
+        }
     }
 }
