bdd85b56f576957ff2c65e31e877b83cf32962bd
wangchao <543164029@qq.com>
2020-03-25 13:40:53 +0800

    添加packet作为client和sever之间的数据消息格式，使用protobuf完成序列化和反序列化，附测试即用例

---
+++ FS_Commons/src/main/java/com/bonree/brfs/common/net/http/data/FSPacketUtil.java
@@ -0,0 +1,15 @@
+package com.bonree.brfs.common.net.http.data;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+/**
+ * @author wangchao
+ */
+public class FSPacketUtil {
+    public static byte[] serialize(FSPacket p){
+        return p.serialize();
+    }
+    public static FSPacket deserialize(byte[] data) throws InvalidProtocolBufferException {
+        return new FSPacket().deserialize(data);
+    }
+}
