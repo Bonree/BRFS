066ca6ce2de5411e5c9d985a638b6ac9d257c873
wangchao <543164029@qq.com>
2020-03-25 16:21:48 +0800

    move the DataTransfer.proto into project FS_Proto

--- FS_Commons/src/main/java/com/bonree/brfs/common/net/http/data/FSPacket.java
+++ FS_Commons/src/main/java/com/bonree/brfs/common/net/http/data/FSPacket.java
@@ -1,7 +1,6 @@
 package com.bonree.brfs.common.net.http.data;
 
-
-import com.bonree.brfs.common.net.http.data.protocal.DataTransferProtos.FSPacketProto;
+import com.bonree.brfs.common.proto.DataTransferProtos.FSPacketProto;
 import com.google.common.base.Preconditions;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
