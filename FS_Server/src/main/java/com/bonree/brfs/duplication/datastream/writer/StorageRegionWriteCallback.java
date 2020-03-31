a11c45c9e14d75118fd219f23fe21d104cb09568
wangchao <543164029@qq.com>
2020-03-31 19:59:48 +0800

    添加单条写入接口

--- FS_Server/src/main/java/com/bonree/brfs/duplication/datastream/writer/StorageRegionWriteCallback.java
+++ FS_Server/src/main/java/com/bonree/brfs/duplication/datastream/writer/StorageRegionWriteCallback.java
@@ -2,5 +2,6 @@ package com.bonree.brfs.duplication.datastream.writer;
 
 public interface StorageRegionWriteCallback {
 	void complete(String[] fids);
+	void complete(String fid);
 	void error();
 }
--- FS_Server/src/main/java/com/bonree/brfs/duplication/datastream/writer/StorageRegionWriter.java
+++ FS_Server/src/main/java/com/bonree/brfs/duplication/datastream/writer/StorageRegionWriter.java
@@ -4,4 +4,5 @@ import com.bonree.brfs.common.write.data.DataItem;
 
 public interface StorageRegionWriter {
 	void write(int storageRegionId, DataItem[] items, StorageRegionWriteCallback callback);
+	void write(int storageRegionId, byte[] data, StorageRegionWriteCallback callback);
 }
