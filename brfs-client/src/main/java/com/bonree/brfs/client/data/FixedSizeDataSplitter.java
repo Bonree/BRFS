a9f66c6e9437cbbb3523b05e8b741bae26f002db
chenyupeng <huizechenyupeng@126.com>
2020-04-01 10:31:33 +0800

    add data splitter

---
+++ brfs-client/src/main/java/com/bonree/brfs/client/data/FixedSizeDataSplitter.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bonree.brfs.client.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.bonree.brfs.client.utils.Strings;
+
+public class FixedSizeDataSplitter implements DataSplitter {
+    private final int maxSize;
+    
+    public FixedSizeDataSplitter(int maxSize) {
+        if(maxSize <= 0) {
+            throw new IllegalArgumentException(Strings.format("max size should > 0, but %d", maxSize));
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public Iterator<ByteBuffer> split(InputStream input) {
+        return new InputStreamIterator(input);
+    }
+    
+    @Override
+    public Iterator<ByteBuffer> split(byte[] bytes) {
+        return new ByteArrayIterator(bytes);
+    }
+
+    private class InputStreamIterator implements Iterator<ByteBuffer> {
+        private final InputStream input;
+        private final byte[] bytes;
+        private int readLength;
+        
+        public InputStreamIterator(InputStream input) {
+            this.input = input;
+            this.bytes = new byte[maxSize];
+        }
+
+        @Override
+        public boolean hasNext() {
+            if(readLength > 0) {
+                return true;
+            }
+            
+            if(readLength < 0) {
+                return false;
+            }
+            
+            try {
+                readLength = input.read(bytes);
+            } catch (IOException e) {
+                readLength = -2;
+            }
+            
+            return readLength >= 0;
+        }
+
+        @Override
+        public ByteBuffer next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            
+            try {
+                return ByteBuffer.wrap(bytes, 0, readLength);
+            } finally {
+                readLength = 0;
+            }
+        }
+        
+    }
+
+    private class ByteArrayIterator implements Iterator<ByteBuffer> {
+        private final ByteBuffer byteBuf;
+        private int offset;
+
+        public ByteArrayIterator(byte[] bytes) {
+            this.byteBuf = ByteBuffer.wrap(bytes);
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return byteBuf.hasRemaining();
+        }
+
+        @Override
+        public ByteBuffer next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            
+            try {
+                ByteBuffer next = byteBuf.duplicate();
+                offset += Math.min(byteBuf.remaining(), maxSize);
+                next.limit(offset);
+                
+                return next;
+            } finally {
+                byteBuf.position(offset);
+            }
+        }
+        
+    }
+}
--- brfs-http-netty/src/test/java/com/bonree/brfs/ServerTest.java
+++ brfs-http-netty/src/test/java/com/bonree/brfs/ServerTest.java
@@ -13,14 +13,28 @@
  */
 package com.bonree.brfs;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
 
 import com.bonree.brfs.http.HttpServer;
 import com.bonree.brfs.jaxrs.JaxrsBinder;
 import com.bonree.brfs.jaxrs.JaxrsModule;
 import com.bonree.brfs.netty.NettyHttpServerModule;
+import com.google.common.base.Throwables;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -36,6 +50,8 @@ public class ServerTest {
             binder.install(new NettyHttpServerModule());
             
             JaxrsBinder.jaxrs(binder).resource(A.class);
+            JaxrsBinder.jaxrs(binder).resource(BProvider.class);
+            JaxrsBinder.jaxrs(binder).resource(EE.class);
         });
         
         HttpServer server = in.getInstance(HttpServer.class);
@@ -48,9 +64,66 @@ public class ServerTest {
     public static class A {
         
         @GET
-        public Response get() {
-            return Response.ok("hello netty\n").build();
+        @Produces(MediaType.APPLICATION_JSON)
+        public B get() {
+//            return new B(99, "HI");
+//            return null;
+            throw new RuntimeException("NONONO");
+        }
+        
+        @GET
+        @Path("r")
+        public Response getR() throws IOException {
+//            return Response.ok().entity("hahah").build();
+            throw new RuntimeException("III");
+        }
+    }
+    
+    public static class B {
+        private int a;
+        private String b;
+        
+        public B(int a, String b) {
+            this.a = a;
+            this.b = b;
+        }
+        
+        public String toJson() {
+            return new StringBuilder()
+                    .append("{")
+                    .append("a : ").append(a).append(",")
+                    .append("b : ").append(b)
+                    .append("}")
+                    .toString();
         }
     }
 
+    @Provider
+    public static class BProvider implements MessageBodyWriter<B> {
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return type == B.class;
+        }
+
+        @Override
+        public void writeTo(B t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+                throws IOException, WebApplicationException {
+            System.out.println("############serialize B");
+            entityStream.write(t.toJson().getBytes(StandardCharsets.UTF_8));
+        }
+        
+    }
+    
+    @Provider
+    public static class EE implements ExceptionMapper<RuntimeException> {
+
+        @Override
+        public Response toResponse(RuntimeException exception) {
+            System.out.println("HERE");
+            return Response.serverError().entity(Throwables.getStackTraceAsString(exception)).build();
+        }
+        
+    }
 }
