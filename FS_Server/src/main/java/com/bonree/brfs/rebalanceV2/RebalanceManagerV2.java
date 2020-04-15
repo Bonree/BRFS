18677ca3a01ba5c1d7948b6b823a7bf639eace1e
bonree.zq <zhangqi@bonree.com>
2020-04-15 16:44:03 +0800

    replace ServerIDManager with IDSManager

--- FS_Server/src/main/java/com/bonree/brfs/rebalanceV2/RebalanceManagerV2.java
+++ FS_Server/src/main/java/com/bonree/brfs/rebalanceV2/RebalanceManagerV2.java
@@ -10,11 +10,12 @@ import com.bonree.brfs.configuration.units.CommonConfigs;
 import com.bonree.brfs.configuration.units.DataNodeConfigs;
 import com.bonree.brfs.configuration.units.RebalanceConfigs;
 import com.bonree.brfs.duplication.storageregion.StorageRegionManager;
+import com.bonree.brfs.identification.IDSManager;
 import com.bonree.brfs.identification.LocalPartitionInterface;
+import com.bonree.brfs.partition.DiskPartitionInfoManager;
 import com.bonree.brfs.rebalanceV2.task.TaskDispatcherV2;
 import com.bonree.brfs.rebalanceV2.task.TaskOperationV2;
 import com.bonree.brfs.rebalanceV2.transfer.SimpleFileServer;
-import com.bonree.brfs.server.identification.ServerIDManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,31 +28,30 @@ import java.util.concurrent.Executors;
 public class RebalanceManagerV2 implements Closeable {
     private final static Logger LOG = LoggerFactory.getLogger(RebalanceManagerV2.class);
 
-    private TaskDispatcherV2 dispatch = null;
-    private TaskOperationV2 opt = null;
+    private TaskDispatcherV2 dispatch;
+    private TaskOperationV2 opt;
     SimpleFileServer fileServer = null;
     ExecutorService simpleFileServer = Executors.newSingleThreadExecutor();
-    private CuratorClient curatorClient = null;
-    private LocalPartitionInterface partitionInterface;
+    private CuratorClient curatorClient;
 
     @Inject
-    public RebalanceManagerV2(ZookeeperPaths zkPaths, ServerIDManager idManager, StorageRegionManager snManager, ServiceManager serviceManager, LocalPartitionInterface partitionInterface) {
-    	this.partitionInterface = partitionInterface;
+    public RebalanceManagerV2(ZookeeperPaths zkPaths, IDSManager idManager, StorageRegionManager snManager, ServiceManager serviceManager, LocalPartitionInterface partitionInterface, DiskPartitionInfoManager partitionInfoManager) {
         String zkAddresses = Configs.getConfiguration().GetConfig(CommonConfigs.CONFIG_ZOOKEEPER_ADDRESSES);
         curatorClient = CuratorClient.getClientInstance(zkAddresses, 500, 500);
         dispatch = new TaskDispatcherV2(curatorClient, zkPaths.getBaseRebalancePath(),
-        		zkPaths.getBaseRoutePath(), idManager,
-        		serviceManager, snManager,
-        		Configs.getConfiguration().GetConfig(RebalanceConfigs.CONFIG_VIRTUAL_DELAY),
-        		Configs.getConfiguration().GetConfig(RebalanceConfigs.CONFIG_NORMAL_DELAY));
-        
+                zkPaths.getBaseRoutePath(), idManager,
+                serviceManager, snManager,
+                Configs.getConfiguration().GetConfig(RebalanceConfigs.CONFIG_VIRTUAL_DELAY),
+                Configs.getConfiguration().GetConfig(RebalanceConfigs.CONFIG_NORMAL_DELAY),
+                partitionInfoManager);
+
 //        String dataPath = Configs.getConfiguration().GetConfig(DataNodeConfigs.CONFIG_DATA_ROOT);
         opt = new TaskOperationV2(curatorClient, zkPaths.getBaseRebalancePath(), zkPaths.getBaseRoutePath(), idManager,
-        		 snManager, serviceManager, partitionInterface);
-        
-		int port = Configs.getConfiguration().GetConfig(DataNodeConfigs.CONFIG_PORT);
+                snManager, serviceManager, partitionInterface);
+
+        int port = Configs.getConfiguration().GetConfig(DataNodeConfigs.CONFIG_PORT);
         try {
-            fileServer = new SimpleFileServer(port + 20, this.partitionInterface, 10);
+            fileServer = new SimpleFileServer(port + 20, partitionInterface, 10);
         } catch (IOException e) {
             LOG.info("fileServer launch error!!!", e);
         }
@@ -76,14 +76,14 @@ public class RebalanceManagerV2 implements Closeable {
     @LifecycleStop
     @Override
     public void close() throws IOException {
-    	simpleFileServer.shutdown();
+        simpleFileServer.shutdown();
 
         if (dispatch != null) {
-        	dispatch.close();
+            dispatch.close();
         }
 
         if (opt != null) {
-        	opt.close();
+            opt.close();
         }
 
         if (fileServer != null) {
