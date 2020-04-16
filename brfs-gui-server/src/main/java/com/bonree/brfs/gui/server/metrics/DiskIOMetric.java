9bba69376c4fd29c06b1ef0aacb104c686dcde84
chenyupeng <huizechenyupeng@126.com>
2020-04-16 16:09:44 +0800

    add resource for gui server

---
+++ brfs-gui-server/src/main/java/com/bonree/brfs/gui/server/metrics/DiskIOMetric.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bonree.brfs.gui.server.metrics;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DiskIOMetric {
+    private final long time;
+    private final double io;
+    
+    @JsonCreator
+    public DiskIOMetric(
+            @JsonProperty("time") long time,
+            @JsonProperty("io") double io) {
+        this.time = time;
+        this.io = io;
+    }
+
+    @JsonProperty("time")
+    public long getTime() {
+        return time;
+    }
+
+    @JsonProperty("usage")
+    public double getIo() {
+        return io;
+    }
+    
+    @Override
+    public String toString() {
+        return toStringHelper(getClass())
+                .add("time", time)
+                .add("io", io)
+                .toString();
+    }
+}
