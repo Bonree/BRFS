95c74c6c9bba4b9dc5e9d91b660988f4a95a03bf
zhucg <zhucg@bonree.com>
2020-05-26 10:39:25 +0800

    fix exception caused by unknown fields when deserializing changes

--- FS_Server/src/main/java/com/bonree/brfs/rebalance/task/DiskPartitionChangeSummary.java
+++ FS_Server/src/main/java/com/bonree/brfs/rebalance/task/DiskPartitionChangeSummary.java
@@ -1,5 +1,6 @@
 package com.bonree.brfs.rebalance.task;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Map;
  * @Author: <a href=mailto:zhangqi@bonree.com>张奇</a>
  * @Description:
  ******************************************************************************/
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DiskPartitionChangeSummary implements Comparable<DiskPartitionChangeSummary> {
 
     @JsonProperty("changeID")
